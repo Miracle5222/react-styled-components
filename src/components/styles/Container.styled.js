54e46a2ef05055d685321d608bc221aa43c7bee0
Roneilgungobbansas <Roneilgungobbansas@gmail.com>
2022-01-16 23:05:32 +0800

    First commit

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+
+
+export const Container = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+min-height: 100vh;
+background: #d3d3d3;
+`
